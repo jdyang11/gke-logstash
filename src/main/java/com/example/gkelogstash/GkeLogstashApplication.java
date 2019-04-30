474737a887fb101cab934273819dfbebd08ef2bc
Danny Yang <danny.yang@equifax.com>
2019-04-30 10:00:41 -0400

    Clean up KkeLogstashApplication.jave

--- src/main/java/com/example/gkelogstash/GkeLogstashApplication.java
+++ src/main/java/com/example/gkelogstash/GkeLogstashApplication.java
@@ -1,24 +1,14 @@
 package com.example.gkelogstash;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class GkeLogstashApplication {
 
-  /*@Value("${spring.application.name}")
-  private String appName;*/
-
   public static void main(String[] args) {
     SpringApplication.run(GkeLogstashApplication.class, args);
   }
-
-  /*@GetMapping("/")
-  public String index() {
-    return "Hello From " + appName + "\n";
-  }*/
 }
